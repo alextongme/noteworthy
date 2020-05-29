28ec475c383f19693ecd2bd1efe86529f270d798
Alex Tong <tongsalex@gmail.com>
2020-05-29 01:26:10 -0400

    implemented redux cycle: actions, reducers, store, for auth

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = APIUtil.login;
-  window.logout = APIUtil.logout;
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
 
