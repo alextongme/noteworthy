6bcf7242ba6409edb87085eac655b0a340109cf3
Alex Tong <tongsalex@gmail.com>
2020-06-12 04:01:05 -0400

    fixed bug editing notebooks

--- src/views/components/Main/Notebooks/NotebookForm/NotebookForm.jsx
+++ src/views/components/Main/Notebooks/NotebookForm/NotebookForm.jsx
@@ -14,11 +14,13 @@ class NotebookForm extends React.Component {
         // debugger
         event.preventDefault();
         const notebook = Object.assign({}, this.state);
-        debugger
-        if(this.props.notebooks.length === 0) {
-            this.props.action(notebook).then((nb) => {
-                this.props.updateDefaultNotebook(nb.notebook.id);
-            })
+        // debugger
+        if(this.props.notebooks) {
+            if(this.props.notebooks.length === 0) {
+                this.props.action(notebook).then((nb) => {
+                    this.props.updateDefaultNotebook(nb.notebook.id);
+                })
+            } 
         } else {
             this.props.action(notebook)
         }
